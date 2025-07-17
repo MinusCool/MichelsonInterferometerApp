7ae3c7389bc403c04bd076ae65f243784f73d5c8
Bagus Heri <bagusheri007@gmail.com>
2025-07-17 23:00:21 +0700

    Ubah Duplikat Konfigurasi MQTTClient

--- composeApp/src/desktopMain/kotlin/org/example/project/MQTTClientDesktop.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/MQTTClientDesktop.kt
@@ -4,26 +4,41 @@ import org.eclipse.paho.client.mqttv3.*
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
 actual object MQTTClient {
-    private const val broker = "ssl://358587e8751a4fbf8655e5a2fb5be16c.s1.eu.hivemq.cloud:8883"
-    private const val clientId = "InterferometerKMPClient"
-    private const val username = "MinusCool"
-    private const val password = "Interfero123"
-    private const val topic = "motor/commands"
-
     private val persistence = MemoryPersistence()
     private var mqttClient: MqttClient? = null
 
+    actual var onMessageReceived: (String) -> Unit = {}
+
     actual fun connect() {
         try {
-            mqttClient = MqttClient(broker, clientId, persistence)
+            mqttClient = MqttClient(MQTTConfig.broker, MQTTConfig.clientId, persistence)
             val connOpts = MqttConnectOptions().apply {
                 isCleanSession = true
-                userName = username
-                password = this@MQTTClient.password.toCharArray()
+                userName = MQTTConfig.username
+                password = MQTTConfig.password.toCharArray()
             }
-            println("Connecting to broker: $broker")
+
+            mqttClient?.setCallback(object : MqttCallback {
+                override fun connectionLost(cause: Throwable?) {
+                    println("Connection lost: ${cause?.message}")
+                }
+
+                override fun messageArrived(topic: String?, message: MqttMessage?) {
+                    val received = message?.toString() ?: ""
+                    println("Message received: $received")
+                    onMessageReceived(received)  // Kirim ke UI
+                }
+
+                override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                    // Tidak perlu diproses untuk sekadar menerima pesan
+                }
+            })
+
+            println("Connecting to broker: $MQTTConfig.broker")
             mqttClient?.connect(connOpts)
-            println("Connected")
+            mqttClient?.subscribe(MQTTConfig.topic)  // Subscribe ke topik saat connect
+            println("Connected and subscribed to $MQTTConfig.topic")
+
         } catch (e: MqttException) {
             println("Error Connecting: ${e.message}")
         }
@@ -40,14 +55,10 @@ actual object MQTTClient {
 
     actual fun publish(message: String) {
         try {
-            mqttClient?.publish(topic, MqttMessage(message.toByteArray()))
+            mqttClient?.publish(MQTTConfig.topic, MqttMessage(message.toByteArray()))
             println("Message published: $message")
         } catch (e: MqttException) {
             println("Error Publishing: ${e.message}")
         }
     }
-
-    actual var onMessageReceived: (String) -> Unit
-        get() = TODO("Not yet implemented")
-        set(value) {}
 }
